bc4747357de49803d8b3a2c56cafdc76d957d878
hhko <hyungho.ko@gmail.com>
2020-04-14 22:07:05 +0900

    관련 링크를 추가한다

---
+++ Books/DesigningWithTypes/Ch02/README.md
@@ -0,0 +1,15 @@
+# Chapter 2. 
+- 
+
+## 목차
+- [Step 1. Introduction](#step1-introduction)
+
+## Step1 Contact 클래스
+- Atomic 데이터 타입을 만들자.
+
+### Stage 1: 리팩토링 전  
+  
+### Stage 2: 리팩토링 후
+
+
+                                                                                               
---
+++ Books/DesigningWithTypes/Ch02/Step1/SingleCaseUnionTypes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SingleCaseUnionTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class EmailAddress
+    {
+        public string Value { get; }
+
+        public EmailAddress(string value)
+        {
+            Value = value;
+        }
+    }
+}
