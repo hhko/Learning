b75c7771040d0a607a4164e9f0d7cafe552fc82f
hhko <hyungho.ko@gmail.com>
2020-04-20 21:20:46 +0900

    Single case union types F# 타입을 추가한다

--- Books/DesigningWithTypes/Ch02/Step1/SingleCaseUnionTypesFS/Program.fs
+++ Books/DesigningWithTypes/Ch02/Step1/SingleCaseUnionTypesFS/Program.fs
@@ -2,31 +2,41 @@
 
 open System
 open PrimitiveTypes
+open Stage2
 
 [<EntryPoint>]
 let main argv =
-    // Stage 1 : Discriminated Unions
-    "x" |> constructedSingular
-    ["a"; "b"; "c"] |> constructedPural 
+    //// Stage 1 : Discriminated Unions
+    //"x" |> constructedSingular
+    //["a"; "b"; "c"] |> constructedPural 
 
-    "x1Let" 
-    |> EmailAddress
-    |> deconstructedSingular1Let        
+    //"x1Let" 
+    //|> EmailAddress
+    //|> deconstructedSingular1Let        
 
-    "x2Pipe" 
-    |> EmailAddress
-    |> deconstructedSingular2Pipe
+    //"x2Pipe" 
+    //|> EmailAddress
+    //|> deconstructedSingular2Pipe
 
-    let x1 = "a" |> EmailAddress
-    x1 |> deconstructedSingular2Pipe
+    //let x1 = "a" |> EmailAddress
+    //x1 |> deconstructedSingular2Pipe
     
-    let address =
-        ["a"; "b"; "c"]
-        |> List.map EmailAddress
+    //let address =
+    //    ["a"; "b"; "c"]
+    //    |> List.map EmailAddress
 
-    let address1 =
-        address
-        |> List.map (fun (EmailAddress e) -> e)
-        |> printfn "%A"
+    //let address1 =
+    //    address
+    //    |> List.map (fun (EmailAddress e) -> e)
+    //    |> printfn "%A"
+
+    //
+    // Stage 2: ...
+    //
+    let s1 = CreateStateCode "CA"
+    let s2 = CreateStateCode "XX"
+    
+    let e1 = CreateEmailAddress "a@example.com"
+    let e2 = CreateEmailAddress "example.com"
         
     0 // return an integer exit code
---
+++ Books/DesigningWithTypes/Ch02/Step1/SingleCaseUnionTypesFS/Stage2/Contact.fs
@@ -0,0 +1,64 @@
+﻿module Stage2
+
+// 이름
+type PersonalName =
+    {
+        FirstName: string;
+        // use "option" to signal optionality
+        MiddleInitial: string; // option;
+        LastName: string;
+    }
+
+type EmailAddress = EmailAddress of string
+
+// 메일
+type EmailContactInfo = 
+    {
+        EmailAddress: EmailAddress;
+        // true if ownership of email address is confirmed
+        IsEmailVerified: bool;
+    }
+
+type ZipCode = ZipCode of string
+type StateCode = StateCode of string
+
+// 주소
+type PostalAddress = 
+    {
+        Address1: string;
+        Address2: string;
+        City: string;
+        State: StateCode;
+        Zip: ZipCode;
+    }
+
+type PostalContactInfo = 
+    {
+        Address: PostalAddress;
+        // true if validated against address service
+        IsAddressValid: bool;
+    }
+
+type Contact = 
+    {
+        // 이름
+        Name: PersonalName;
+
+        // 메일
+        EmailContactInfo: EmailContactInfo;
+    
+        // 주소
+        PostalContactInfo: PostalContactInfo;
+    }
+
+let CreateEmailAddress (s: string) =
+    if System.Text.RegularExpressions.Regex.IsMatch(s, @"^\S+@\S+\.\S+$")
+        then Some (EmailAddress s)
+        else None
+
+let CreateStateCode (s: string) = 
+    let s' = s.ToUpper()
+    let stateCodes = ["AZ"; "CA"; "NY"] // etc
+    if stateCodes |> List.exists ((=) s')
+        then Some (StateCode s')
+        else None
