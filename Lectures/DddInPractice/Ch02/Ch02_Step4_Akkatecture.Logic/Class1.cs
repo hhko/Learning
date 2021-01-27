e0dd564118fbd61f28c24bd5a727a2478cd2ecc9
hhko <hyungho.ko@gmail.com>
2021-01-27 17:41:12 +0900

    불필요한 프로젝트 삭제

--- Lectures/DddInPractice/Ch02/Ch02_Step4_Akkatecture.Logic/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Ch02_Step4_Akkatecture.Logic
-{
-    public class Class1
-    {
-    }
-}
--- Lectures/DddInPractice/Ch02/Ch02_Step4_Akkatecture.UnitTests/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Ch02_Step4_Akkatecture.UnitTests
-{
-    public class Class1
-    {
-    }
-}
