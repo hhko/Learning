18b3831cd2705f6f1a217649b5083b9f4d00e4ef
hhko <hyungho.ko@gmail.com>
2021-02-15 07:00:24 +0900

    Ch07 예제 추가 중

---
+++ 2.Lectures/DDD/DddInPractice/Ch07/Ch07_Step1_DomainEvent/Common/DomainEvents.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch07_Step1_DomainEvent.Common
+{
+    public static class DomainEvents
+    {
+        private static List<Type> _handlers;
+
+        public static void Init()
+        {
+            _handlers = Assembly.GetExecutingAssembly()
+                .GetTypes()
+                .Where(x => x.GetInterfaces().Any(y => y.GetGenericTypeDefinition() == typeof(IHandler<>)))
+                .ToList();
+        }
+
+        public static void Dispatch(IDomainEvent domainEvent)
+        {
+            foreach (Type handlerType in _handlers)
+            {
+                bool canHandleEvent = handlerType.GetInterfaces()
+                    .Any(x => x.IsGenericType
+                        && x.GetGenericTypeDefinition() == typeof(IHandler<>)
+                        && x.GenericTypeArguments[0] == domainEvent.GetType());
+
+                if (canHandleEvent)
+                {
+                    dynamic handler = Activator.CreateInstance(handlerType);
+                    handler.Handle((dynamic)domainEvent);
+
+                    //var x = (IStore)Activator.CreateInstance(constructedType, new object[] { someParameter });
+                }
+            }
+        }
+    }
+}
