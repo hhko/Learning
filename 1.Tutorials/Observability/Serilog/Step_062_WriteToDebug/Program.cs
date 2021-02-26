cbdc14da404ae5533fb4ca63dced71fc8884d25d
hhko <hyungho.ko@gmail.com>
2021-02-26 13:33:22 +0900

    Architectural Principles 업데이트

--- 1.Tutorials/Observability/Serilog/Step_062_WriteToDebug/Program.cs
+++ 1.Tutorials/Observability/Serilog/Step_062_WriteToDebug/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Serilog;
+using System;
+using System.Threading;
 
 namespace Step_062_WriteToDebug
 {
@@ -6,7 +8,27 @@ namespace Step_062_WriteToDebug
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .WriteTo.Debug()
+                .CreateLogger();
+
+            try
+            {
+                Log.Debug("Getting started");
+
+                Log.Information("Hello {Name} from thread {ThreadId}", Environment.GetEnvironmentVariable("USERNAME"), Thread.CurrentThread.ManagedThreadId);
+
+                Log.Warning("No coins remain at position {@Position}", new { Lat = 25, Long = 134 });
+
+                throw new DivideByZeroException("예외 발생하기");
+            }
+            catch (Exception e)
+            {
+                Log.Error(e, "Something went wrong");
+            }
+
+            Log.CloseAndFlush();
         }
     }
 }
